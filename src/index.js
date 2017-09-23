d769a737b49968096ae225bda9c02e010f2debbb
Ivan Grushevich <githubmail911@gmail.com>
2017-09-23 15:10:19 +0200

    implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var dictionary = {};
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    var arr = bracketsConfig[i];
+    dictionary[ arr[0] ] = arr[1];
+  }
+
+  var bufferArr = [];
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i];
+    if(char in dictionary) {
+      bufferArr.push(char);
+    } else if ( char == dictionary[ bufferArr[bufferArr.length-1] ] ) {
+      bufferArr.pop();
+    } else return false;
+  }
+  return (!bufferArr.length) ? true : false;
 }
